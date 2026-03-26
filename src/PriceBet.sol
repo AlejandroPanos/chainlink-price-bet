b827b9f0d800a306314ac483bcf2554f4a1283e7
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-03-26 18:02:47 +0100

    [add] - Chainlink contracts

--- src/PriceBet.sol
+++ src/PriceBet.sol
@@ -1,6 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+/**
+ * @dev A contract where two players can bet ETH against each other on whether
+ * the price of ETH will be above or below a target USD price at a specific point in time. 
+ * The first player sets the target price and picks a side. 
+ * The second player takes the opposite side. 
+ * When the time is up, anyone can trigger the settlement and the winner takes the pot. 
+ * @dev Chainlink gets implemented to access real world data.
+ */
 contract PriceBet {
-    
+
+    /* Errors */
+
+    /* Type declarations */
+
+    /* State variables */
+
+    /* Events */
+
+    /* Constructor */
+
+    /* Functions */
+
+    /* Getter functions */
+
 }
