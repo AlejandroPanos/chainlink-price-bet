ccab56f67245bbaf0cc3659d53b794178ed9831a
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-03-27 18:51:49 +0100

    [add] - Deploymet script

--- script/DeployPriceBet.s.sol
+++ script/DeployPriceBet.s.sol
@@ -2,6 +2,21 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
 import {PriceBet} from "src/PriceBet.sol";
 
-contract DeployPriceBet is Script {}
+contract DeployPriceBet is Script {
+    function run() external returns (PriceBet) {
+        // Get the active network config from the helper config
+        HelperConfig helperConfig = new HelperConfig();
+        address activePriceFeed = helperConfig.activeNetworkConfig();
+
+        // Deploy the PriceBet.sol contract
+        vm.startBroadcast();
+        PriceBet priceBet = new PriceBet(activePriceFeed);
+        vm.stopBroadcast();
+
+        // Return the contract
+        return priceBet;
+    }
+}
