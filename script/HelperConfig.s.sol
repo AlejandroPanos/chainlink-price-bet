e918eb0eb2e1696930d03f699d9397a74f25bf79
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-03-27 18:28:15 +0100

    [add] - Chain configs

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,7 +2,30 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "test/mocks/MockV3Aggregator.t.sol";
 
 contract HelperConfig is Script {
-    /* Initial declarations */
+    /* Type declarations */
+    struct NetworkConfig {
+        address priceFeed;
+    }
+
+    /* State variables */
+    NetworkConfig public activeNetworkConfig;
+    uint256 public constant DECIMALS = 8;
+    uint256 public constant INITIAL_ANSWER = 2000e8;
+
+    /* Constructor */
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaConfig();
+        } else {
+            activeNetworkConfig = getAnvilConfig();
+        }
+    }
+
+    /* Functions */
+    function getSepoliaConfig() public returns (NetworkConfig memory) {}
+
+    function getAnvilConfig() public returns (NetworkConfig memory) {}
 }
